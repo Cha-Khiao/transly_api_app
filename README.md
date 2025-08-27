ac605eb39f2aeeddaf5afc985245839c64e2eb93
Cha-Khiao <p.prasopphol203@gmail.com>
2025-08-27 12:48:08 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# transly_api_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/transly_api_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.transly_api_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
